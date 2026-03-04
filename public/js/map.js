e480c29539496467b45c3c5038db9d6c9482954a
khushantchopkar123 <khushantchopkar646@gmail.com>
2026-03-05 00:44:30 +0530

    update in code

--- public/js/map.js
+++ public/js/map.js
@@ -1,28 +1,28 @@
 
 //stored in env file for secutity
 
+mapboxgl.accessToken = mapToken;
 
-  mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates,
+  zoom: 4,
+});
 
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v12",
-    center:listing.geometry.coordinates,
-    zoom: 4,
-  });
+map.addControl(new mapboxgl.NavigationControl());
 
-  // <!-- ✅ ADD THIS CODE RIGHT HERE -->
-  const marker = new mapboxgl.Marker({
-    color: "red",
-    draggable: true
-  })
-    .setLngLat(listing.geometry.coordinates)
-    .setPopup(
-      new mapboxgl.Popup({ offset: 25 }).setHTML(
-        `<h4>${listing.location}</h4><p>Exact Location provided after booking</p>`
-      )
+const marker = new mapboxgl.Marker({
+  color: "red",
+})
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h5>${listing.location}</h5>
+       <p>Exact location provided after booking</p>`
     )
-    .addTo(map);
+  )
+  .addTo(map);
 
  
 
